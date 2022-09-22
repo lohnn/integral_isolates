3a409e8343edefc00de62eed4bac8f9f00786433
lohnn <johannes@lohnn.se>
2022-09-22 20:13:06 +0200

    Export exceptions

--- packages/integral_isolates/lib/integral_isolates.dart
+++ packages/integral_isolates/lib/integral_isolates.dart
@@ -2,4 +2,6 @@ export 'package:integral_isolates/src/backpressure/backpressure_strategy.dart';
 export 'package:integral_isolates/src/backpressure/discard_new_backpressure_strategy.dart';
 export 'package:integral_isolates/src/backpressure/no_backpressure_strategy.dart';
 export 'package:integral_isolates/src/backpressure/replace_backpressure_strategy.dart';
+export 'package:integral_isolates/src/exceptions/drop_exception.dart';
+export 'package:integral_isolates/src/exceptions/init_exception.dart';
 export 'package:integral_isolates/src/integral_isolates.dart';
