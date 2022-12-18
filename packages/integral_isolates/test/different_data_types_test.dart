dddbd5bb97e5b60a838653dfda14f9b0df3a5a9b
lohnn <johannes@lohnn.se>
2022-12-18 15:47:24 +0100

    Handle sending unsupported data types without crashing everything

---
+++ packages/integral_isolates/test/different_data_types_test.dart
@@ -0,0 +1,56 @@
+import 'dart:isolate';
+
+import 'package:integral_isolates/integral_isolates.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Exceptions when queuing jobs should not break upcoming jobs', () {
+    final isolated = Isolated();
+    final isolate = isolated.isolate;
+
+    test('Send unsupported data type should throw exception', () async {
+      expect(
+        await isolate((number) => number + 2, 1),
+        equals(3),
+      );
+
+      await expectLater(
+        isolate(
+          print,
+          ReceivePort(),
+        ),
+        throwsArgumentError,
+      );
+
+      expect(
+        await isolate((number) => number + 2, 5),
+        equals(7),
+      );
+    });
+
+    test('Trying to return unsupported data type should throw exception',
+        () async {
+      expect(
+        await isolate((number) => number + 8, 1),
+        equals(9),
+      );
+
+      await expectLater(
+        isolate(
+          (_) {
+            return ReceivePort();
+          },
+          'Test String',
+        ),
+        throwsArgumentError,
+      );
+
+      expect(
+        await isolate((number) => number + 5, 25),
+        equals(30),
+      );
+    });
+
+    tearDownAll(isolated.dispose);
+  });
+}
