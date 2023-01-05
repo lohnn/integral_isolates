a97738e1e12f9cb3c5d1577eb390f6cd4cdae69a
lohnn <johannes@lohnn.se>
2023-01-05 16:03:56 +0100

    Test backpressure strategy for dynamic isolated with different types

--- packages/integral_isolates/test/back_pressure_strategy_test.dart
+++ packages/integral_isolates/test/back_pressure_strategy_test.dart
@@ -8,27 +8,40 @@ int _testFunction(int input) {
   return input;
 }
 
+String _testFunctionStr(String text) {
+  sleep(const Duration(milliseconds: 100));
+  return 'prefix: $text';
+}
+
 void main() {
   group('Simple iterable responses from backpressure', () {
-    List<int> iterable() => [1, 2, 3, 4, 5].toList();
+    List iterable() => [1, 'test', 2, 3, 4, 5].toList();
 
-    Future<List<int>> runIsolate(BackpressureStrategy strategy) async {
+    Future<List> runIsolate(BackpressureStrategy strategy) async {
       final isolated = Isolated(
         backpressureStrategy: strategy,
         autoInit: false,
       );
       await isolated.init();
 
-      final responses = <int>[];
+      final responses = [];
 
-      Future temp(int number) async {
-        await Future.delayed(Duration(milliseconds: 50 * number));
-
-        try {
-          final value = await isolated.isolate(_testFunction, number);
-          responses.add(value);
-        } catch (e) {
-          // Noop
+      Future temp(dynamic input) async {
+        if (input is String) {
+          try {
+            final value = await isolated.isolate(_testFunctionStr, input);
+            responses.add(value);
+          } catch (e) {
+            // Noop
+          }
+        } else if (input is int) {
+          await Future.delayed(Duration(milliseconds: 50 * input));
+          try {
+            final value = await isolated.isolate(_testFunction, input);
+            responses.add(value);
+          } catch (e) {
+            // Noop
+          }
         }
       }
 
@@ -43,31 +56,34 @@ void main() {
 
     test('Default (no) strategy', () async {
       final isolate = Isolated();
-      final responses = <int>[];
+      final responses = [];
 
       await Future.wait([
-        for (final int in iterable())
-          isolate.isolate(_testFunction, int).then(responses.add),
+        for (final input in iterable())
+          if (input is String)
+            isolate.isolate(_testFunctionStr, input).then(responses.add)
+          else if (input is int)
+            isolate.isolate(_testFunction, input).then(responses.add),
       ]);
 
       isolate.dispose();
 
-      expect(responses, [1, 2, 3, 4, 5]);
+      expect(responses, [1, 'prefix: test', 2, 3, 4, 5]);
     });
 
     test('No backpressure strategy', () async {
       final responses = await runIsolate(NoBackPressureStrategy());
-      expect(responses, [1, 2, 3, 4, 5]);
+      expect(responses, ['prefix: test', 1, 2, 3, 4, 5]);
     });
 
     test('Discard new backpressure strategy', () async {
       final responses = await runIsolate(DiscardNewBackPressureStrategy());
-      expect(responses, [1, 2]);
+      expect(responses, ['prefix: test', 1, 3]);
     });
 
     test('Replace backpressure strategy', () async {
       final responses = await runIsolate(ReplaceBackpressureStrategy());
-      expect(responses, [1, 5]);
+      expect(responses, ['prefix: test', 2, 5]);
     });
   });
 }
