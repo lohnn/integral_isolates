2c572774dfee132a19b2c8dfd4c4af99a37eb96d
lohnn <johannes@lohnn.se>
2023-04-16 19:56:27 +0200

    Barrel export for use_isolate

--- packages/use_isolate/example/lib/main.dart
+++ packages/use_isolate/example/lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:use_isolate/use_isolate.dart';
-import 'package:use_isolate/use_tailored_isolate.dart';
 
 void main() {
   runApp(const MaterialApp(home: OuterWidget()));
