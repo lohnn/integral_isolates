d93f9466b0ddb8ebce386704753efe7142464d1f
lohnn <johannes@lohnn.se>
2023-09-05 13:53:28 +0200

    Better toString on UnexpectedDropException

--- packages/integral_isolates/lib/src/exceptions/drop_exception.dart
+++ packages/integral_isolates/lib/src/exceptions/drop_exception.dart
@@ -9,4 +9,12 @@ class IsolateClosedDropException extends DropException {}
 
 /// Exception that preferably should never occur, but that will be thrown when
 /// the isolate call was dropped in unexpected cases.
-class UnexpectedDropException extends DropException {}
+class UnexpectedDropException extends DropException {
+  @override
+  String toString() {
+    return 'Exception: This is an unexpected internal error that should not not'
+        ' have been possible. Please file an issue at '
+        'https://github.com/lohnn/integral_isolates/issues with a reproducible '
+        'example.';
+  }
+}
