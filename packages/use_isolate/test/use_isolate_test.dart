ff9e4337920e5015a774212ca28968d4df478b1e
lohnn <johannes@lohnn.se>
2023-12-29 15:37:04 +0100

    Added some tests for use_isolate that tests compute and computeStream for the two variants

--- packages/use_isolate/test/use_isolate_test.dart
+++ packages/use_isolate/test/use_isolate_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:use_isolate/src/use_isolate.dart';
+import 'package:use_isolate/use_isolate.dart';
 
 void main() {
   testWidgets('Testing lifecycle of StatefulIsolate from useIsolate',
@@ -34,6 +34,61 @@ void main() {
           'message',
         ),
         throwsA(isA<IsolateClosedDropException>()),
+      );
+    });
+  });
+
+  testWidgets('compute function works for StatefulIsolate from useIsolate',
+      (tester) async {
+    await tester.runAsync(() async {
+      late StatefulIsolate isolate;
+
+      await tester.pumpWidget(
+        HookBuilder(
+          builder: (context) {
+            isolate = useIsolate();
+            return Container();
+          },
+        ),
+      );
+
+      await expectLater(
+        isolate.compute(
+          (input) => input * input,
+          4,
+        ),
+        completion(16),
+      );
+    });
+  });
+
+  testWidgets(
+      'computeStream function works for StatefulIsolate from useIsolate',
+      (tester) async {
+    await tester.runAsync(() async {
+      late StatefulIsolate isolate;
+
+      await tester.pumpWidget(
+        HookBuilder(
+          builder: (context) {
+            isolate = useIsolate();
+            return Container();
+          },
+        ),
+      );
+
+      await expectLater(
+        isolate.computeStream(
+          (input) =>
+              Stream.fromIterable(Iterable.generate(4, (i) => '$input$i')),
+          'message',
+        ),
+        emitsInOrder([
+          'message0',
+          'message1',
+          'message2',
+          'message3',
+        ]),
       );
     });
   });
--- packages/use_isolate/test/use_tailored_isolate_test.dart
+++ packages/use_isolate/test/use_tailored_isolate_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:use_isolate/src/use_tailored_isolate.dart';
+import 'package:use_isolate/use_isolate.dart';
 
 void main() {
   testWidgets(
@@ -39,4 +39,59 @@ void main() {
       });
     },
   );
+
+  testWidgets(
+      'compute function works for TailoredStatefulIsolate from useTailoredIsolate',
+      (tester) async {
+    await tester.runAsync(() async {
+      late TailoredStatefulIsolate<String, int> isolate;
+
+      await tester.pumpWidget(
+        HookBuilder(
+          builder: (context) {
+            isolate = useTailoredIsolate();
+            return Container();
+          },
+        ),
+      );
+
+      await expectLater(
+        isolate.compute(
+          (input) => input.length,
+          'message',
+        ),
+        completion(7),
+      );
+    });
+  });
+
+  testWidgets(
+      'computeStream function works for TailoredStatefulIsolate from useTailoredIsolate',
+      (tester) async {
+    await tester.runAsync(() async {
+      late TailoredStatefulIsolate<int, String> isolate;
+
+      await tester.pumpWidget(
+        HookBuilder(
+          builder: (context) {
+            isolate = useTailoredIsolate();
+            return Container();
+          },
+        ),
+      );
+
+      await expectLater(
+        isolate.computeStream(
+          (input) =>
+              Stream.fromIterable(Iterable.generate(input, (i) => 'message$i')),
+          3,
+        ),
+        emitsInOrder([
+          'message0',
+          'message1',
+          'message2',
+        ]),
+      );
+    });
+  });
 }
