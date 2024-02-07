6d9b5376462f8052c619cd852dd33acbb68094fb
lohnn <johannes@lohnn.se>
2024-02-07 09:40:02 +0100

    Added an integration_test that works with calling native code

---
+++ packages/flutter_integral_isolates/integration_test/init_test.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_integral_isolates/flutter_integral_isolates.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void _initPluginForIsolate(RootIsolateToken rootIsolateToken) {
+  BackgroundIsolateBinaryMessenger.ensureInitialized(rootIsolateToken);
+}
+
+Future<String?> _readSharedPreferences(Object? _) async {
+  final sharedPreferences = await SharedPreferences.getInstance();
+  return sharedPreferences.getString('store');
+}
+
+Future<void> _writeSharedPreferences(String value) async {
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sharedPreferences.setString('store', value);
+}
+
+void main() {
+  setUp(() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.clear();
+  });
+  tearDown(() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.clear();
+  });
+
+  testWidgets(
+    'Running platform code works in isolate',
+    (tester) async {
+      await tester.runAsync(() async {
+        await tester.pumpWidget(Container());
+        final isolate = StatefulIsolate();
+
+        final rootIsolateToken = RootIsolateToken.instance!;
+        await isolate.compute(_initPluginForIsolate, rootIsolateToken);
+
+        await expectLater(
+          isolate.compute(
+            _readSharedPreferences,
+            null,
+          ),
+          completion(null),
+        );
+
+        await expectLater(
+          isolate.compute(
+            _writeSharedPreferences,
+            'Some data!',
+          ),
+          completes,
+        );
+
+        await expectLater(
+          isolate.compute(
+            _readSharedPreferences,
+            null,
+          ),
+          completion(equals('Some data!')),
+        );
+      });
+    },
+  );
+}
---
+++ packages/flutter_integral_isolates/lib/flutter_integral_isolates.dart
@@ -0,0 +1,1 @@
+export 'package:integral_isolates/integral_isolates.dart';
